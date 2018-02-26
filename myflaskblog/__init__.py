e2e487f6ff4bf1f62b576a908980298dd36ba072
10000ms <243449042@qq.com>
2018-02-26 18:14:16 +0800

    copy the sample

---
+++ myflaskblog/__init__.py
@@ -0,0 +1,1 @@
+from .myflaskblog import app
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='myflaskblog',
+    packages=['myflaskblog'],
+    include_package_data=True,
+    install_requires=[
+        'myflaskblog',
+    ],
+)
