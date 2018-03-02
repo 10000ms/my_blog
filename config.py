60bcaa7833e8c2e9ddf3ab1ae3a287bb65bb6496
10000ms <243449042@qq.com>
2018-03-02 15:45:14 +0800

    修改设置模块

--- config.py
+++ config.py
@@ -7,6 +7,30 @@ __author__ = 'Victor Lai'
 配置模块
 '''
 
-config ={
-        'SQLALCHEMY_DATABASE_URI': '',
-        }
+
+class Config_Default(object):
+    DEBUG = False
+    TESTING = False
+
+
+class ProductionConfig(Config_Default):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class DevelopmentConfig(Config_Default):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost:3306/test'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+
+class TestingConfig(Config_Default):
+    TESTING = True
+
+
+config = {
+    'config': DevelopmentConfig,
+}
