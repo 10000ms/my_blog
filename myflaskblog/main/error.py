1bfe72f169a3081ce65c22c6b6d2114c997f0cf3
Victor Lai <243449042@qq.com>
2018-03-06 22:16:45 +0800

    正确添加错误处理模块

--- myflaskblog/main/error.py
+++ myflaskblog/main/error.py
@@ -7,27 +7,26 @@ __author__ = 'Victor Lai'
 出错页面模块
 '''
 
-# 导入蓝图模块
-from flask import Blueprint
-
-error = Blueprint('error', __name__)
+# 导入项目模块
+from myflaskblog import app
 
 
-@error.errorhandler(404)
-def index_page():
+# error处理模块
+@app.errorhandler(404)
+def error_404_page(error):
     return '404 Not Found', 404
 
 
-@error.errorhandler(403)
-def index_page():
+@app.errorhandler(403)
+def error_403_page(error):
     return '403 Forbidden', 403
 
 
-@error.errorhandler(410)
-def index_page():
+@app.errorhandler(410)
+def error_410_page(error):
     return '410 Gone', 410
 
 
-@error.errorhandler(500)
-def index_page():
+@app.errorhandler(500)
+def error_500_page(error):
     return '500 Internal Server Error', 500
