d56f49fe15fa89481b5fc87ed910f0ed746b07c4
10000ms <243449042@qq.com>
2018-03-18 18:04:25 +0800

    分页模块

--- myflaskblog/main/index.py
+++ myflaskblog/main/index.py
@@ -15,13 +15,18 @@ from flask import render_template
 
 # 导入必要的模块
 from myflaskblog.models import User, Article
+from flask import request
 
 index = Blueprint('index', __name__)
 
 
-@index.route('/')
+@index.route('/', methods=['GET', 'POST'])
 def index_page():
-    return render_template("/index/index.html")
+    page = request.args.get('page', 1, type=int)
+    pagination = Article.query.order_by(Article.create_datetime.desc()).paginate(
+        page, per_page=10, error_out=True)
+    articles = pagination.items
+    return render_template("/index/index.html", articles=articles, pagination=pagination)
 
 
 @index.route('/search')
