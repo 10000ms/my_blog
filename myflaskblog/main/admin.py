4dad88e1465f4a61ef5926acb45c8a5e040d5d14
10000ms <243449042@qq.com>
2018-03-22 15:17:35 +0800

    用户删除功能

--- myflaskblog/main/admin.py
+++ myflaskblog/main/admin.py
@@ -14,8 +14,9 @@ from flask import Blueprint
 from flask_login import login_user, login_required, logout_user, current_user
 
 # 导入必须的模块
-from flask import request, flash, url_for, render_template, redirect
+from flask import request, flash, url_for, render_template, redirect, abort
 from myflaskblog.models import User
+from myflaskblog import db
 
 admin = Blueprint('admin', __name__)
 
@@ -24,3 +25,28 @@ admin = Blueprint('admin', __name__)
 @login_required
 def admin_index_page():
     return render_template('/admin/admin.html')
+
+
+@admin.route('/user_manage')
+@login_required
+def user_manage_page():
+    if current_user.is_admin == 1:
+        all_user = User.query.filter(User.is_admin != 1)
+        page = request.args.get('page', 1, type=int)
+        pagination = all_user.paginate(page, per_page=10, error_out=True)
+        users = pagination.items
+        return render_template("/admin/user_manage.html", users=users, pagination=pagination)
+    else:
+        abort(403)
+
+
+@admin.route('/delete_user/<int:user_id>', methods=['POST'])
+@login_required
+def delete_user_page(user_id):
+    if current_user.is_admin == 1:
+        delete_user = User.query.filter_by(id=user_id).first()
+        db.session.delete(delete_user)
+        db.session.commit()
+        return '删除成功'
+    else:
+        abort(403)
