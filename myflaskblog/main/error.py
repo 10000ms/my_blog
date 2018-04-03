d692a3f98f19b62962d5da0588bec040a977c613
10000ms <243449042@qq.com>
2018-04-03 18:06:00 +0800

    错误页面及页面前端优化

--- myflaskblog/main/error.py
+++ myflaskblog/main/error.py
@@ -7,9 +7,11 @@ __author__ = 'Victor Lai'
 出错页面模块
 '''
 
-# 导入项目模块
-# from myflaskblog import app
+# 导入蓝图模块
 from flask import Blueprint
+
+# 导入模板模块
+from flask import render_template
 
 # error处理模块
 # @app.errorhandler(404)
@@ -34,21 +36,26 @@ from flask import Blueprint
 
 error_handler = Blueprint('error', __name__)
 
+
 @error_handler.app_errorhandler(404)
 def error_404_page(error):
-    return '404 Not Found', 404
+    return render_template('error/error.html', error_title=404, error_1=4, error_2=0, error_3=4, \
+            error_message='该页面不存在'), 404
 
 
 @error_handler.app_errorhandler(403)
 def error_403_page(error):
-    return '403 Forbidden', 403
+    return render_template('error/error.html', error_title=403, error_1=4, error_2=0, error_3=3, \
+            error_message='资源不可用'), 403
 
 
-@error_handler.app_errorhandler(410)
-def error_410_page(error):
-    return '410 Gone', 410
+@error_handler.app_errorhandler(401)
+def error_401_page(error):
+    return render_template('error/error.html', error_title=410, error_1=4, error_2=0, error_3=1, \
+            error_message='没有权限'), 401
 
 
 @error_handler.app_errorhandler(500)
 def error_500_page(error):
-    return '500 Internal Server Error', 500
+    return render_template('error/error.html', error_title=500, error_1=5, error_2=0, error_3=0, \
+            error_message='服务器内部错误'), 500
