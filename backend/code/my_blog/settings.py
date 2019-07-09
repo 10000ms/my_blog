6af25e92882b20c923b1bfa247a56037536e0ffb
10000ms <243449042@qq.com>
2019-07-09 17:30:07 +0800

    [IMP]增加部署配置

--- backend/code/my_blog/settings.py
+++ backend/code/my_blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,9 +80,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'my_blog',
-        # 'TEST': {
-        #        'NAME': 'test_my_blog',
-        # },
         'USER': secret.DATABASE_ACCOUNT,
         'PASSWORD': secret.DATABASE_PASSWORD,
         'HOST': 'mysql',
